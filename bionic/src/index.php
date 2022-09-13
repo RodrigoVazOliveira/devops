275ac526e4ac4180c279261985f9d43bc2ff678d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-09-13 09:58:48 -0300

    Usando puppet e validando o vagrantfile

---
+++ bionic/src/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo 'Testando a conexao com o banco de dados! <br />';
+
+$host = '192.168.1.15';
+$username = 'phpuser';
+$password = 'pass';
+$configConnection = "mysql:host={$host};";
+
+try {
+	$connection = new \PDO($configConnection, $username, $password);
+	echo 'conexao estabelecida com sucesso ao banco de dados MySQL! <br />';
+} catch (\PDOException $e) {
+	echo "nao foi possivel conectar ao banco de dados {$e->getMessage()} <br />";
+	die();
+}
+
+?>
