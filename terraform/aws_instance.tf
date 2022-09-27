4459fecdc0bfeee789277167d4ab03ab80a13385
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2022-09-26 21:30:02 -0300

    remover recursos do AWS com comentarios

--- terraform/aws_instance.tf
+++ terraform/aws_instance.tf
@@ -9,16 +9,16 @@ resource "aws_instance" "dev" {
     vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
 }
 
-resource "aws_instance" "dev4" {
-    ami = var.amis["us-east-1"]
-    instance_type = "t2.micro"
-    key_name = var.key_ssh_file
-    tags = { 
-        Name = "dev4"
-    }
-    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
-    depends_on = [aws_s3_bucket.dev-4]
-}
+#resource "aws_instance" "dev4" {
+#    ami = var.amis["us-east-1"]
+#    instance_type = "t2.micro"
+#    key_name = var.key_ssh_file
+#    tags = { 
+#        Name = "dev4"
+#    }
+#    vpc_security_group_ids = ["${aws_security_group.acesso_ssh.id}"]
+#    depends_on = [aws_s3_bucket.dev-4]
+#}
 
 resource "aws_instance" "dev5" {
     ami = var.amis["us-east-1"]
